6c99748a721ec589ea853692f08f298aedd02504
Igor Korovchenko <igsekor@gmail.com>
2026-04-03 13:58:52 +0300

    Добавляет файлы для хранения данных заметок и источников

--- practice-5-7/config/config.py
+++ practice-5-7/config/config.py
@@ -1,6 +1,8 @@
 AUTHOR_FILE = 'data/author.dat'
+NOTE_FILE = 'data/note.dat'
 PUBLISHER_FILE = 'data/publisher.dat'
 RECORD_FILE = 'data/record.dat'
+SOURCE_FILE = 'data/source.dat'
 
 
 def get_config():
